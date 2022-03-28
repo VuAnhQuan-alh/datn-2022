ccbe2c0fc47a626e7a3c5b01ee214932f430c26c
Vu Anh Quan <quava2521@gmail.com>
2022-03-28 19:38:49 +0700

    remove build folder

--- build/static/js/12.9c719598.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[12],{411:function(a,e,n){"use strict";n.r(e);n(0);var t=n(3);e.default=function(){return Object(t.jsx)("div",{children:"ThirdPage components"})}}}]);
-//# sourceMappingURL=12.9c719598.chunk.js.map
